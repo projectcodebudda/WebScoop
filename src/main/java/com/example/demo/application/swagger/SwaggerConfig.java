764cf66fa500eb75db9323561b51044b26167e53
김동욱 <dongwook9763@naver.com>
2024-08-23 22:32:13 +0900

    add : kafka Api 엔드포인트 추가

---
+++ src/main/java/com/example/demo/application/swagger/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.application.swagger;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Springdoc")
+                .description("Springdoc을 사용한 Swagger UI")
+                .version("0.0.1");
+    }
+}
--- src/main/java/com/example/demo/presentation/api/v1/kafka/KafkaProducerController.java
+++ src/main/java/com/example/demo/presentation/api/v1/kafka/KafkaProducerController.java
@@ -1,9 +1,13 @@
 package com.example.demo.presentation.api.v1.kafka;
 
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/kafka")
 public class KafkaProducerController {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
@@ -12,7 +16,8 @@ public class KafkaProducerController {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(String json) {
+    @PostMapping("/send")
+    public void sendMessage(@RequestBody String json) {
         kafkaTemplate.send("my-topic", json);
     }
 }
